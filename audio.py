ea3da7886b1bc909cea73f81294d72b7e8428c38
Henry Haefliger <54628404+hnhaefliger@users.noreply.github.com>
2019-10-03 12:02:53 +0200

    Add files via upload

--- audio.py
+++ audio.py
@@ -12,7 +12,7 @@ CHUNK = int(RATE/20)
 
 stream = mic.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, output=True, frames_per_buffer=CHUNK)
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(14,6))
 
 x = np.arange(0, 2 * CHUNK, 2)
 ax.set_ylim(-200, 200)
