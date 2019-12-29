9dea7835e2bbe0da6173e5cfbf6fd2d2c69c09fd
Henry Haefliger <54628404+hnhaefliger@users.noreply.github.com>
2019-12-29 13:30:11 +0100

    Add files via upload

--- spectrogram.py
+++ spectrogram.py
@@ -6,10 +6,12 @@ from scipy import signal
 
 mic = pyaudio.PyAudio()
 
+INTERVAL = 0.32
+
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 10000
-CHUNK = int(RATE/20)
+RATE = 48000
+CHUNK = int(RATE * INTERVAL)
 
 stream = mic.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, output=True, frames_per_buffer=CHUNK)
 
@@ -17,12 +19,13 @@ plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
 
 while True:
-    data = stream.read(CHUNK)
-    data = np.array(struct.unpack(str(2 * CHUNK) + 'B', data), dtype='b')
+    data = stream.read(CHUNK, exception_on_overflow=False)
+    data = np.frombuffer(data, dtype='b')
     f, t, Sxx = signal.spectrogram(data, fs=CHUNK)
     dBS = 10 * np.log10(Sxx)
+    plt.clf()
     plt.pcolormesh(t, f, dBS)
-    plt.pause(0.005)
+    plt.pause(0.001)
     
 stream.stop_stream()
 stream.close()
--- waves.py
+++ waves.py
@@ -15,16 +15,16 @@ stream = mic.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, outpu
 
 fig, ax = plt.subplots(figsize=(14,6))
 
-ax.set_ylim(0, 200)
+ax.set_ylim(-200, 200)
 ax.set_xlim(0, CHUNK)
 
 x = np.arange(0, 2 * CHUNK, 2)
 line, = ax.plot(x, np.random.rand(CHUNK))
 
 while True:
-    data = stream.read(CHUNK)
+    data = stream.read(CHUNK, exception_on_overflow=False)
     data = np.array(struct.unpack(str(2 * CHUNK) + 'B', data), dtype='b')[::2]
-    data = abs(data)
+    #data = abs(data)
     line.set_ydata(data)
     fig.canvas.draw()
     fig.canvas.flush_events()
